1e4dfe2b2a79f8db3af030f8603e55d0508cfea7
Bozheniu <bozheniu@gmail.com>
2022-10-24 00:28:30 +0200

    add ExampleSeminar2

---
+++ Example008_Arrays/Program.cs
@@ -0,0 +1,69 @@
+﻿/* int[] array = { 221, 342, 30, 445, 58, 18, 27, 18, 4329 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+*/
+
+// Если метод (функция) ничего не возвращает, он называется void методом.
+
+void FillArray(int[] collection)
+
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // чтобы не возвращался 0 при поиске слишком большого числа, это можно интерпретировать, как такой элемент не найден
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 5);
+Console.WriteLine(pos);
---
+++ ExampleSeminar_2/Program.cs
@@ -0,0 +1,80 @@
+﻿/* Напишите прогрумму, которая принимает на вход трехзначное число
+и на выходе показывает последнюю цифру этого числа.*/
+
+/* 
+int GetLastDigit(int number)
+{
+    //int lastDigit = number % 10;
+    //return lastDigit;
+    return number % 10; // сокращенная строчка
+}
+
+int testNumber = 456;
+// int lastDigit = GetLastDigit(testNumber);
+// Console.WriteLine(lastDigit);
+Console.WriteLine(GetLastDigit(testNumber));
+*/
+
+/* var rnd = new Random(); // var позволяет автоматически определить
+// тип, который будет у нас в переменной
+// var num = 32;
+
+rnd.Next();
+
+int b = rnd.Next();
+*/
+
+/*
+void DoNothing()
+{
+    Console.WriteLine("Start!");
+    return;
+    Console.WriteLine("End!");
+
+}
+
+DoNothing();
+*/
+
+/* 
+for (int i = 0; i < 100; i++)
+{
+    if (i == 20)
+        break; // break используется для прерывания циклов
+}
+*/
+/*
+for (int i = 0; i < 10; i++)
+{
+    if (i == 5)
+        continue; // пропускаем одну итерацию в цикле
+    Console.WriteLine(i);
+
+}
+*/
+
+/*
+Напишите программу, которая выводит случайное
+число из отрезка [10,99] и показывает наибольшую цифру числа.
+
+int BiggerDigit(int number)
+{
+    int result = -1;
+    int one = number % 10;
+    int two = number / 10;
+
+    if (one > two)
+    {
+        result = one;
+    }
+    else
+    {
+        result = two;
+    }
+    return result;
+}
+
+int numberRandom = new Random().Next(10, 100);
+Console.WriteLine(numberRandom);
+Console.WriteLine(BiggerDigit(numberRandom));
+*/
